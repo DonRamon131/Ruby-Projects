721c6a8e938227859804a8021a7e4092b71fd8b7
Steven <stevenescobar131@gmail.com>
2023-01-11 16:50:33 -0600

    working on project

--- caesar_cypher.rb
+++ caesar_cypher.rb
@@ -3,16 +3,18 @@
 end
 string_show_byte("String")
 =end
+#puts "#{c} = #{ (c.ord - shift).chr }"
+        #if (c.ord - shift < min_val)
 
 def caesar_cypher(string, shift)
     new_string = ""
-
     string.each_char do |c|
-        new_string += (c.ord - shift).chr
-        #puts "#{c} = #{ (c.ord - shift).chr }"
-        #if (c.ord - shift < min_val)
-    end
-    print "#{string} =  #{new_string}" 
+        new_string +=  (c.ord + shift).chr
+        if (c.ord + shift) > 122
+            new_string += (c.ord + shift - 26).chr
+        end
+    print "#{string} = #{new_string}\n" 
 end
 
-caesar_cypher("vwxyz", 5)
+caesar_cypher("abcfg", 2) #xyz{|
+caesar_cypher("abcde", 2) #cdefg
