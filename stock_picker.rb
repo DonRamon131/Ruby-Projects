bc1be4ba0fb9cef130f1282cb4efef5f135d69cd
Steven <stevenescobar131@gmail.com>
2023-01-28 15:07:58 -0600

    projects finished

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,6 +1,12 @@
 def stock_picker(array)
-    new_array = array.reverse()
-    new_array.map ||
+
+    if array.find_index(array.max()) > array.find_index(array.min())
+        puts "min at:#{array.find_index(array.min())} max at:#{array.find_index(array.max())}"
+    elsif array.find_index(array.max()) < array.find_index(array.min())
+        
+    end
+    
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
+stock_picker([10,1,6,8,15,9,6,3,17])
