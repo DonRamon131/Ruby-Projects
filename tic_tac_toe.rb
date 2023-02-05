7f74964262171bdee3330c20bcb3b822fe17e1e6
Steven <stevenescobar131@gmail.com>
2023-02-05 12:11:27 -0600

    working on project

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,13 +1,5 @@
-table =  "1|2|3\n4|5|6\n7|8|9"
-print table
-=begin
-      "1|2|3
-       4|5|6
-       7|8|9"
-=end
-
 module Players
-   def writeable()
+   def writeable(place)
    end
 
    def initialize(name ,char)
@@ -16,22 +8,39 @@ module Players
    end
    
    def return_name_and_char()
-      print "\n#{@name} will be playing first as: #{@char}"
+      "#{@name} will be playing as: #{@char}"
    end
 end
 
 class Player1
    include Players 
-   
 end
 
 class Player2
    include Players
-   
 end
 
-steven = Player1.new('Steven','X')
-somebody = Player2.new('Somebody','O')
-#print "Steven will be playing with #{Player1.print_char} as first, and Somebody will be playing with #{Player2.print_char} as second"
-print steven.return_name_and_char
-print somebody.return_name_and_char
+def game_loop()
+   table =  "1|2|3\n4|5|6\n7|8|9\n"
+=begin
+      "1|2|3
+       4|5|6
+       7|8|9"
+=end
+   print "\nEnter name of first player: "
+   first_name = gets.chomp
+   puts "\nEnter character: "
+   first_char = gets.chomp
+   print "\nEnter name of second player: "
+   second_name = gets.chomp
+   puts "\nEnter character: "
+   second_char = gets.chomp
+
+   first_player = Player1.new(first_name,first_char)
+   second_player = Player2.new(second_name,second_char)
+   #p "#{first_player.return_name_and_char}#{second_player.return_name_and_char}"
+   print "\n" + first_player.return_name_and_char + ", " + second_player.return_name_and_char + "\n"
+   print table
+end
+
+game_loop()
